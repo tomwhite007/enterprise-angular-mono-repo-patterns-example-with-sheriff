c8f812de4b539a2e3f6006e6018974c70b4ac328
Tom White <tomwhite007@gmail.com>
2024-10-24 08:38:13 +0100

    feat: move check-in-desktop-e2e

--- apps/check-in/check-in-desktop-e2e/eslint.config.js
+++ apps/check-in/check-in-desktop-e2e/eslint.config.js
@@ -1,5 +1,5 @@
 const playwright = require('eslint-plugin-playwright');
-const baseConfig = require('../../eslint.config.js');
+const baseConfig = require('../../../eslint.config.js');
 
 module.exports = [
   playwright.configs['flat/recommended'],
