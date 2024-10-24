e9562650a3c4437eff38c5d3ee1395a4dda87981
Tom White <tomwhite007@gmail.com>
2024-10-24 08:36:36 +0100

    feat: move booking-mobile

--- apps/booking/booking-mobile/jest.config.ts
+++ apps/booking/booking-mobile/jest.config.ts
@@ -1,8 +1,8 @@
 export default {
   displayName: 'booking-mobile',
-  preset: '../../jest.preset.js',
+  preset: '../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/booking/booking-mobile',
+  coverageDirectory: '../../../coverage/apps/booking/booking-mobile',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
